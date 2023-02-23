7742fa730255d41e43a274651082085b3b449553
Jean Santos <santosjeandx@gmail.com>
2023-02-23 15:45:38 -0500

    CRUD with API and React Tables

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # CRUD With API
 
-<!-- Extraido de: https://www.bezkoder.com/react-typescript-api-call/ -->
-
-[DEMO](https://server-crud.onrender.com/)
+<!-- Extraido de: https://www.bezkoder.com/react-table-example-hooks-crud/ -->
+<!-- Extraido de: https://github.com/bezkoder/react-table-crud-example -->
 
 Instalaciones:
 * React Router Dom
 * Axios
 * Bootstrap
+* [React Table](https://www.npmjs.com/package/react-table)
+* FontAwesome
 
 ```
 npm i react-router-dom
@@ -15,6 +16,15 @@ npm i react-router-dom
 npm i axios
 
 npm i bootstrap@5.3.0-alpha1
+
+npm i react-table
+npm i --save-dev @types/react-table
+```
+
+```
+<!-- IMPORT FONTAWESOME INDEX.HTML -->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 ```
 
 # Screenshots
--- src/components/TutorialsList.tsx
+++ src/components/TutorialsList.tsx
@@ -1,21 +1,29 @@
 // Hooks and Types React
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useRef, useState } from "react";
 // React Router DOM
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // Services
-import { findItemByTitle, getAllItem, removeAllItems } from "../services/TutorialService";
+import { findItemByTitle, getAllItem, removeAllItems, removeItem } from "../services/TutorialService";
 // Interfaces
-import ITutorialData from "../types/Tutorial";
+import ITutorialData from '../types/Tutorial';
+// Components
+import { Search } from "./Search";
+// React tables
+import { CellProps, CellValue, Column, useTable } from 'react-table'
+// Sweet Alert
+import Swal from "sweetalert2";
 
 export const TutorialsList = () => {
 
   const [tutorials, setTutorials] = useState<Array<ITutorialData>>([]);
-  const [currentTutorial, setCurrentTutorial] = useState<ITutorialData | null>(null);
-  const [currentIndex, setCurrentIndex] = useState<number>(-1);
-  const [searchTitle, setSearchTitle] = useState<string>("");
+  const [searchTitle, setSearchTitle] = useState<string>('');
+  const tutorialsRef = useRef<Array<ITutorialData>>([]);
+  let navigate = useNavigate();
+
+  tutorialsRef.current = tutorials;
 
   useEffect(() => {
-    retrieveTutorials();
+    retrieveTutorials()
   }, []);
 
   const onChangeSearchTitle = (e: ChangeEvent<HTMLInputElement>) => {
@@ -33,13 +41,6 @@ export const TutorialsList = () => {
 
   const refreshList = () => {
     retrieveTutorials();
-    setCurrentTutorial(null);
-    setCurrentIndex(-1);
-  };
-
-  const setActiveTutorial = (tutorial: ITutorialData, index: number) => {
-    setCurrentTutorial(tutorial);
-    setCurrentIndex(index);
   };
 
   const removeAllTutorials = () => {
@@ -54,100 +55,126 @@ export const TutorialsList = () => {
     findItemByTitle(searchTitle)
       .then(({ data }) => {
         setTutorials(data);
-        setCurrentTutorial(null);
-        setCurrentIndex(-1);
       })
       .catch((e: Error) => console.log(e))
   };
 
+  const openTutorial = (rowIndex: number) => {
+    const id = tutorialsRef.current[rowIndex].id;
+    navigate('/tutorials/' + id)
+  };
+
+  const deleteTutorial = (rowIndex: number) => {
+    const id = tutorialsRef.current[rowIndex].id;
+    removeItem(id)
+      .then(() => {
+        Swal.fire(`${tutorialsRef.current[rowIndex].title} successfully deleted.`, '', 'success');
+
+        navigate('/tutorials')
+        let newTutorials = [...tutorialsRef.current];
+        newTutorials.splice(rowIndex, 1);
+
+        setTutorials(newTutorials);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const columns: Column<ITutorialData>[] = useMemo(() => [
+    {
+      Header: 'Title',
+      accessor: 'title' as keyof ITutorialData,
+    },
+    {
+      Header: 'Description',
+      accessor: 'description' as keyof ITutorialData,
+    },
+    {
+      Header: 'Status',
+      accessor: 'published' as keyof ITutorialData,
+      Cell: ({ value }: CellProps<ITutorialData>): CellValue => {
+        return value ? 'Published' : 'Pending';
+      }
+    },
+    {
+      Header: 'Actions',
+      accessor: 'actions' as CellValue,
+      Cell: ({ row }: CellProps<ITutorialData>): JSX.Element => {
+        const rowIdx = row.id;
+        return (
+          <div className='d-flex justify-content-center'>
+            <span onClick={() => openTutorial(parseInt(rowIdx))}>
+              <i className="far fa-edit action mx-2"></i>
+            </span>
+            <span onClick={() => deleteTutorial(parseInt(rowIdx))}>
+              <i className="fas fa-trash action mx-2"></i>
+            </span>
+          </div>
+        )
+      }
+    }
+    // eslint-disable-next-line
+  ], [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    prepareRow,
+    headerGroups,
+    rows,
+  } = useTable<ITutorialData>({ columns, data: tutorials })
 
   return (
     <div className="list row">
-      <div className="col-12">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by title"
-            value={searchTitle}
-            onChange={onChangeSearchTitle}
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-secondary"
-              type="button"
-              onClick={findByTitle}
-            >
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-6">
-        <h4>Tutorials List</h4>
 
-        <ul className="list-group">
-          {
-            tutorials &&
-            tutorials.map((tutorial, index) => (
-              <li
-                className={
-                  "list-group-item " + (index === currentIndex ? "active" : "")
-                }
-                onClick={() => setActiveTutorial(tutorial, index)}
-                key={index}
-              >
-                {tutorial.title}
-              </li>
-            ))
-          }
-        </ul>
+      <Search searchTitle={searchTitle} onChangeSearchTitle={onChangeSearchTitle} findByTitle={findByTitle} />
 
-        <button
-          className="m-3 btn btn-danger"
-          onClick={removeAllTutorials}
+      <div className="col-md-12 list">
+        <table
+          className="table table-striped table-bordered table-hover"
+          {...getTableProps()}
         >
+          <thead>
+            {
+              headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {
+                    headerGroup.headers.map((column) => (
+                      <th {...column.getHeaderProps()}>
+                        {column.render("Header")}
+                      </th>
+                    ))
+                  }
+                </tr>
+              ))
+            }
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {
+              rows.map((row, i) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {
+                      row.cells.map((cell) => {
+                        return (
+                          <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                        );
+                      })
+                    }
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+
+      <div className="col-md-8">
+        <button className="btn btn-sm btn-danger" onClick={removeAllTutorials}>
           Remove All
         </button>
-      </div>
-      <div className="col-md-6">
-        {
-          currentTutorial ? (
-            <div>
-              <h4>Tutorial</h4>
-              <div>
-                <label>
-                  <strong>Title:</strong>
-                </label>{" "}
-                {currentTutorial.title}
-              </div>
-              <div>
-                <label>
-                  <strong>Description:</strong>
-                </label>{" "}
-                {currentTutorial.description}
-              </div>
-              <div>
-                <label>
-                  <strong>Status:</strong>
-                </label>{" "}
-                {currentTutorial.published ? "Published" : "Pending"}
-              </div>
-
-              <Link
-                to={"/tutorials/" + currentTutorial.id}
-                className="btn btn-warning mt-3"
-              >
-                Edit
-              </Link>
-            </div>
-          ) : (
-            <div>
-              <br />
-              <p>Please click on a Tutorial...</p>
-            </div>
-          )
-        }
       </div>
     </div>
   )
